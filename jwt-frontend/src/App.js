9a7fd67055836810c3de9ac00c448fbb4cb6de92
ezacoantonio <ezacoantonio@gmail.com>
2023-11-08 21:05:36 -0500

    v1.0

---
+++ jwt-frontend/src/App.js
@@ -0,0 +1,13 @@
+import {Button} from '@material-ui/core'; 
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button> Press me </Button> //using the material ui component in our project
+    </div>
+  );
+}
+
+export default App;
